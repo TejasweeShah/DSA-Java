2a24559a938c12512fddf1e4c4639aea3e4029f5
Tejaswee Shah <tejaswee.shah@gmail.com>
2022-03-15 10:42:48 -0400

    Hello World

---
+++ src/com/tsl/Main.java
@@ -0,0 +1,15 @@
+package com.tsl;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Your Name : ");
+        String inputStr = scanner.next();
+        System.out.println("Hello! "+ inputStr);
+    }
+
+}
