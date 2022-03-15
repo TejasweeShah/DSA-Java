da50002b9cf34cc6c9328133a039bf0e405d614d
Tejaswee Shah <tejaswee.shah@gmail.com>
2022-03-15 15:41:33 -0400

    Update Fibonacci.java
    Update : nth Fibonacci no with function
    F(0) = 0, F(1) = 1
    F(n) = F(n - 1) + F(n - 2), for n > 1.

--- src/com/tsl/basics/Fibonacci.java
+++ src/com/tsl/basics/Fibonacci.java
@@ -2,6 +2,30 @@ package com.tsl.basics;
 
 import java.util.Scanner;
 
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+Example 1:
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+ */
+
 public class Fibonacci {
 
     public static void main(String[] args) {
@@ -45,9 +69,9 @@ public class Fibonacci {
     }
 
     private void printNthNo(int nthNo) {
-        int no1 = 0, no2 = 1, count = 1;
+        int no1 = 0, no2 = 1, count = 0;
 
-        while (count != nthNo) {
+        while (count < nthNo) {
             int tempSum = no1 + no2;
             no1 = no2;
             no2 = tempSum;
