2d5183d55dec0736c34abe7fb9238d6fd5be7651
Tejaswee Shah <tejaswee.shah@gmail.com>
2022-03-15 10:58:22 -0400

    Reverse the given number

---
+++ src/com/tsl/basics/ReverseNumber.java
@@ -0,0 +1,23 @@
+package com.tsl.basics;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Digit");
+        long inputNo = input.nextLong();
+        long reverse = 0;
+
+        while (inputNo > 0) {
+            int digit = (int) inputNo % 10;
+            reverse = reverse * 10 + digit;
+
+            inputNo = inputNo / 10;
+        }
+
+        System.out.println("Reverse Number is : " + reverse);
+    }
+
+}
